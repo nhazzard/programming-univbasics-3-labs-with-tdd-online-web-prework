36f561c13e9bb2b15bd6687e644dd4f811f18e4c
Nicholas Hazzard <nickhazzard@ymail.com>
2019-10-20 18:43:56 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
-# Add your variables here
+print "1";
+# get the input from the console,
+first_number = 1;
+print "2";
+second_number = 2;
+# convert the string console inputs to_i (to_integers) and add together
+print "Answer : " , (first_number.to_i + second_number.to_i), "\n";
+sum = (first_number+second_number)
+difference = (first_number-second_number)
+product = (first_number*second_number)
+quotient = (first_number/second_number)
