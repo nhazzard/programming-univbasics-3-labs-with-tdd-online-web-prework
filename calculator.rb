eccaab27e7b06e18f3ca2828fc0b43355b4eb30d
Nicholas Hazzard <nickhazzard@ymail.com>
2019-10-20 18:31:40 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
-# Add your variables here
+print "1";
+# get the input from the console, 
+val1 = gets;
+print "2";
+val2 = gets;
+# convert the string console inputs to_i (to_integers) and add together
+print "Answer : " , (val1.to_i + val2.to_i), "\n";
