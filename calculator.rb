66282ac356a43c74520a930a6cc3ca4a9778a485
Nicholas Hazzard <nickhazzard@ymail.com>
2019-10-20 18:43:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -8,3 +8,4 @@ print "Answer : " , (first_number.to_i + second_number.to_i), "\n";
 sum = (first_number+second_number)
 difference = (first_number-second_number)
 product = (first_number*second_number)
+quotient = (first_number/second_number)
