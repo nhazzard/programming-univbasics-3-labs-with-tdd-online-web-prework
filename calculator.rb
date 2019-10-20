6a16c275a96de81f672153df76e76518d8c1d83c
Nicholas Hazzard <nickhazzard@ymail.com>
2019-10-20 18:41:09 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,3 +5,4 @@ print "2";
 second_number = 2;
 # convert the string console inputs to_i (to_integers) and add together
 print "Answer : " , (first_number.to_i + second_number.to_i), "\n";
+sum = (first_number+second_number)
